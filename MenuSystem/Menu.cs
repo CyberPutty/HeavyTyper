2c39281496619f560c8a4a4c8580a2ec1dc05e34
Michael <michaelcordero88@gmail.com>
2020-02-23 22:36:00 -0800

    added Menu class, requires header, options, footer

--- MenuSystem/Menu.cs
+++ MenuSystem/Menu.cs
@@ -4,13 +4,99 @@ using System.Text;
 
 namespace HeavyTyper.MenuSystem
 {
-    class Menu
+    abstract class Menu
     {
-
-
-       public void Print() 
+        public string header;
+        public List<Option> options;
+        int selectedIndex = 0;
+        public string footer;
+        public ConsoleColor selectedColor = ConsoleColor.Green;
+        public ConsoleColor unselectedColor= ConsoleColor.Cyan;
+        public Menu(string header,List<Option> options,string footer)
         {
-            Console.WriteLine("Menu");
+            this.header = header;
+            this.footer = footer;
+            this.options = options;
+            this.options[0].color = selectedColor;
+        }
+       public void Print() 
+        {          
+            Console.Clear();
+            Console.CursorVisible = false;
+            if (header.Length > 0) 
+            {
+                Console.WriteLine(header);
+            }
+            
+            foreach (Option option in options) 
+            {
+                Console.ForegroundColor = option.color;
+                Console.WriteLine(option.text);
+                
+            }
+            Console.ResetColor();
+            if (footer.Length>0) 
+            {
+                Console.WriteLine(footer);
+            }
+            
+            GetInput(Console.ReadKey());
+        }
+        public void GetInput(ConsoleKeyInfo key) 
+        {
+            switch (key.Key) 
+            {
+                case ConsoleKey.UpArrow:
+                    if (selectedIndex > 0)
+                    {
+                        MoveUp();
+                        Print();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Out of bounds sound");
+                        Print();
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (selectedIndex < options.Count-1)
+                    {
+                        MoveDown();
+                        Print();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Out of bounds sound");
+                        Print();
+                    }
+                    break;
+                case ConsoleKey.Enter:
+                    Run();
+                    break;
+                case ConsoleKey.Q:
+                    return;
+                default:
+                    Print();
+                    break;
+                  
+            }
+        }
+        public string Run()
+        {
+            return options[selectedIndex].text;
+        }
+
+        public void MoveUp() 
+        {
+            options[selectedIndex].color = unselectedColor;
+            this.selectedIndex = selectedIndex - 1;
+            options[selectedIndex].color = selectedColor;
+        }
+        public void MoveDown() 
+        {
+            options[selectedIndex].color = unselectedColor;
+            this.selectedIndex = selectedIndex + 1;
+            options[selectedIndex].color = selectedColor;
         }
     }
 }
---
+++ MenuSystem/Option.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeavyTyper.MenuSystem
+{
+    class Option
+    {
+        public string text="DefaultOption";
+        public ConsoleColor color = ConsoleColor.Cyan;
+    }
+}
